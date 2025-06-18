4b94b5ef104b4c97964d4018f86133b88ba2f116
jligh <jlightrs@gmail.com>
2025-06-18 18:04:48 -0500

    Trying to add course files

---
+++ assignments/Lesson 1 Assignment/Lesson 1 Assignment.py
@@ -0,0 +1,20 @@
+# "hello"
+# 'pizza'
+# apple = "bannana"
+# apple
+# tree = 'bannana'
+# tree == apple
+# hippo = tree
+# cheese = lettuce
+# 12 = 42
+# 451 + 613
+# 12 - 54
+# 12 * 12
+# 144/12
+# t = 3
+# r = 9/3
+# t == r
+# 83 > 5
+# 83 > 83
+# 83 >= 83
+# print("Completed by, James Light")
